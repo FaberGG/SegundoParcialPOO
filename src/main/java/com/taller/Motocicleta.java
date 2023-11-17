b4f1ebf752ffa4a16b8159cd63dddf18121a2ee6
jhon faber navia narvaez <jhonnavia999@hotmail.com>
2023-11-17 01:42:29 -0500

    Primer Commit

---
+++ src/main/java/com/taller/Motocicleta.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.taller;
+/**
+ *
+ * @author 104623010352 John Faber Navia Narvaez
+ */
+public class Motocicleta extends Vehiculo {
+    private int cilindrada;
+
+    // Constructores, getters y setters
+
+    // Otros métodos específicos para motocicletas
+
+    public Motocicleta(int cilindrada, String marca, String modelo, String placa, String horaIngreso) {
+        super(marca, modelo, placa, horaIngreso);
+        this.cilindrada = cilindrada;
+    }
+    
+    public void setCilindrada(int cilindrada){
+        this.cilindrada = cilindrada;
+    }
+    public int getCilindrada(){
+        return this.cilindrada;
+    }
+}
